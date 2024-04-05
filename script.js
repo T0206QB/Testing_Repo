67df58da08c33ad79b92ddaf852add5dbfd09b00
T0183MT <rahini.g@stellantis.com>
2024-04-05 17:30:50 +0530

    WP-2986: show only DD alert msg during DD and ensure games grid and no games msg are shown aptly

--- script.js
+++ script.js
@@ -21,13 +21,13 @@ window.addEventListener("no_driver_distraction", () => {
 
 function load_driver_distraction() {
   document.getElementById('ddmessage').textContent = top.locale['dd_games_message'];
-  $("#gameContainer").hide();
+  $(".games-flex-container").css("display", "none");
   $("#ddmessage").css("display", "flex");
 }
 
 function load_non_driver_distraction() {
   $("#ddmessage").css("display", "none");
-  $("#gameContainer").show();
+  $(".games-flex-container").css("display", "flex");
 }
 
 $(document).ready(function () {
